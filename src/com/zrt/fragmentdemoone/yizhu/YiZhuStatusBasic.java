498b9ffa1752ba60e23cf6beef77a7dd801eb515
zrt <zrt@192.168.2.105>
2017-05-18 00:38:42 +0800

    bb

--- src/com/zrt/fragmentdemoone/yizhu/YiZhuStatusBasic.java
+++ src/com/zrt/fragmentdemoone/yizhu/YiZhuStatusBasic.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.zrt.fragmentdemoone.GlobalInfoApplication;
 
 public abstract class YiZhuStatusBasic {
+	public String yizhu_type = "全部";
 	
 	public GlobalInfoApplication current_application;
 	
@@ -16,6 +17,13 @@ public abstract class YiZhuStatusBasic {
 	public abstract String getSQLiteType(String yizhu_type);
 	
 	public abstract <T> void onExecute(T object);
+	
+	/**
+	 * 2017-05-15
+	 * @param yizhu_type 医嘱类型
+	 * @param isUpdate 是否刷新医嘱
+	 */
+	public abstract void setYiZhuType(String yizhu_type, boolean isUpdate);
 	
 	public <T> List<T> getData(){
 		
